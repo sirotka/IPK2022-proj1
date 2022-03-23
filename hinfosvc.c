8770dadcfb44719269e8ffdfdbd65a9911349a95
my name is pain <71525300+sirotka@users.noreply.github.com>
2022-03-23 22:30:48 +0100

    Add files via upload

--- hinfosvc.c
+++ hinfosvc.c
@@ -23,9 +23,7 @@ void getHostname(char *string)
 
 void getCpuLoad(char *string)
 {
-    //FILE *cpu_load = popen("grep 'cpu ' /proc/stat > file1 | sleep 1 && grep 'cpu ' /proc/stat > file2 | cat file1 file2 |awk -v RS=\"\" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5) \"%\"}' ", "r");
-
-    FILE *cpu_load = popen("grep 'cpu ' /proc/stat | sleep 1 && grep 'cpu ' /proc/stat | awk -v RS=\"\" '{print ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5) \"%\"}' ", "r");
+    FILE *cpu_load = popen("top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1\"%\"}'", "r");
 
     fgets(string, MAX_BUFFER, cpu_load);
     fclose(cpu_load);
